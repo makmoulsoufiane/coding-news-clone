c9656700fb1616d0ac50ad3c540eeb2a96824d77
makmoul soufiane <makmoulsoufiane2@gmail.com>
2025-06-22 16:48:32 +0100

    add and fix bug feature story user can sumbit url and title

--- apps/story/views.py
+++ apps/story/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .forms import StoryForm
@@ -11,6 +11,16 @@ def frontpage(request):
 
 @login_required
 def submit(request):
+    if request.method == 'POST':
         form = StoryForm(request.POST)
 
-        return render(request, 'story/submit.html', {'form': form})
+        if form.is_valid():
+            story = form.save(commit=False)
+            story.created_by = request.user
+            story.save()
+
+            return redirect('frontpage')  # Redirect to the frontpage after submission
+    else:
+        form = StoryForm()
+
+    return render(request, 'story/submit.html', {'form': form})
