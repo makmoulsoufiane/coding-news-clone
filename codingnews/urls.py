ffe4d275d2f359a5f0996e745464d9992397fedb
makmoul soufiane <makmoulsoufiane2@gmail.com>
2025-06-22 13:58:33 +0100

    Initial commit: Django project setup

---
+++ codingnews/urls.py
@@ -0,0 +1,16 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views
+from apps.story.views import frontpage
+from apps.core.views import signup
+
+urlpatterns = [
+    path('', frontpage, name='frontpage'),
+    path('signup/', signup, name='signup'),
+    path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('admin/', admin.site.urls),
+
+]
+ 
